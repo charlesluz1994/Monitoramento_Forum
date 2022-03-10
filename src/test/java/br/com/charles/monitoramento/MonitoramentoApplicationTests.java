576df4ce63ccbc9f152516568481982e946c4fc8
Charles Luz <charlesluz.94@gmail.com>
2022-03-10 14:21:32 +0000

    First Commit

---
+++ src/test/java/br/com/charles/monitoramento/MonitoramentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.charles.monitoramento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoramentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
